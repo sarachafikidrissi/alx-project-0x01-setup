f1e6efd0b6b13b6c8811c8262d7b9a5c303a6072
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-12-22 17:10:35 +0100

    add button + footer

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+      <footer className="bg-gray-800 text-white p-4 mt-4">
+        <p className="text-center">© 2024 My Website</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
